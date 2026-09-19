094bd5137bc30da48fd79340eb91b92887938b30
Saanvi <saanvi.mukund2026@vitstudent.ac.in>
2026-09-19 12:50:49 +0530

    added comment

--- calci.py
+++ calci.py
@@ -11,7 +11,9 @@ def multiply(a, b):
 def divide(a, b):
     if b == 0:
         return "Error: Cannot divide by zero"
-    return a / b
+    return a / b  
+  #comment
+
 
 def calculator():
     print("===== Simple Calculator =====")
